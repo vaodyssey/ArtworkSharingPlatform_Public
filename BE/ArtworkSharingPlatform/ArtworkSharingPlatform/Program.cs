282b2dadc704c5fa3038f27e6d87dbe5c875ffef
tanutv123 <taitanvo2309@gmail.com>
2024-02-26 14:30:00 +0700

    Add session

--- BE/ArtworkSharingPlatform/ArtworkSharingPlatform/Program.cs
+++ BE/ArtworkSharingPlatform/ArtworkSharingPlatform/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSession();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: artworkSharingPlatformCors,
