5ff8470cfa04483c128302c8473648ea2e0d63f2
tanutv123 <taitanvo2309@gmail.com>
2024-02-27 14:15:59 +0700

    Add landing page template

--- FE/ArtworkSharingPlatform/src/app/app.module.ts
+++ FE/ArtworkSharingPlatform/src/app/app.module.ts
@@ -7,12 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./_modules/shared.module";
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {LoadingInterceptor} from "./_interceptor/loading.interceptor";
+import {PricingComponent} from "./components/artist/pricing/pricing.component";
+import { ArtworkComponent } from './components/artwork/artwork.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotfoundComponent } from './components/error/notfound/notfound.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    PricingComponent,
+    ArtworkComponent,
+    ContactComponent,
+    LoginComponent,
+    NotfoundComponent,
+    SignupComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +36,11 @@ import { NavComponent } from './components/nav/nav.component';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,
+    useClass: LoadingInterceptor,
+    multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
