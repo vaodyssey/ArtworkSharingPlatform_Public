b8196c05f3e7aa4d46d9ddc3e9f188568632fac1
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-03-06 00:31:23 +0700

    Add register, login function

---
+++ BE/ArtworkSharingPlatform/ArtworkSharingPlatform.DataTransferLayer/LoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtworkSharingPlatform.DataTransferLayer
+{
+    public class LoginDTO
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =6)]
+        public string Password { get; set; }
+    }
+}
---
+++ BE/ArtworkSharingPlatform/ArtworkSharingPlatform.DataTransferLayer/RegisterDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtworkSharingPlatform.DataTransferLayer
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$", ErrorMessage = "Username can only contain letters, digits, and spaces.")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmedPassword { get; set; }
+        public byte Status { get; set; }
+
+    }
+}
