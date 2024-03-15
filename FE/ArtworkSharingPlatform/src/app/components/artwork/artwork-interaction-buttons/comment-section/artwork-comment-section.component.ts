164f833094e00451bfb46012b2c6e5925058727c
vathuglife <thuandt582@gmail.com>
2024-03-15 22:57:50 +0700

    vathuglife: fixed seed.cs and refactored Artwork Details UI

--- FE/ArtworkSharingPlatform/src/app/components/artwork/artwork-interaction-buttons/comment-section/artwork-comment-section.component.ts
+++ FE/ArtworkSharingPlatform/src/app/components/artwork/artwork-interaction-buttons/comment-section/artwork-comment-section.component.ts
@@ -1,64 +1,66 @@
-import { Component,OnInit,Input,Inject } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Artwork } from 'src/app/_model/artwork.model';
 import { SharedModule } from 'src/app/_modules/shared.module';
 import { CommentService } from 'src/app/_services/_comment.service';
 import { ArtworkNewComment } from 'src/app/_model/artworkNewComment.model';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 import { ArtworkComment } from 'src/app/_model/artworkComment.model';
 @Component({
     selector: 'app-artwork-comment-section',
     templateUrl: './artwork-comment-section.component.html',
     styleUrls: ['./artwork-comment-section.component.css'],
-    standalone:true,
-    imports:[CommonModule,SharedModule,FormsModule]
+    standalone: true,
+    imports: [CommonModule, SharedModule, FormsModule]
 })
 export class ArtworkCommentSectionComponent implements OnInit {
-    @Input() artwork: Artwork|undefined
-    commentInput:string = ""
+    @Input() artwork: Artwork | undefined
+    commentInput: string = ""
     commentsData: ArtworkComment[] = [];
-    
 
-    constructor(private commentService:CommentService){        
+
+    constructor(private commentService: CommentService) {
     }
     ngOnInit(): void {
         this.getCommentsFromDatabase()
     }
-    async getCommentsFromDatabase(){
+    async getCommentsFromDatabase() {
         console.log("Trying to retrieve the latest comments for this artwork...")
         this.commentService.getComments(this.artwork?.id as number)
-        .subscribe((response)=>{ 
-                                
-            let tempArr = response as ArtworkComment[]
-            for(let i = 0; i < tempArr.length; i++) {
-                let jsonComment = tempArr[i];                                
-                let eachComment = {
-                    userId:jsonComment["userId"],
-                    artworkId:jsonComment["artworkId"],
-                    content:jsonComment["content"]
+            .subscribe((response) => {
+
+                let tempArr = response as ArtworkComment[]
+                for (let i = 0; i < tempArr.length; i++) {
+                    let jsonComment = tempArr[i];
+                    let eachComment = {
+                        userId: jsonComment["userId"],
+                        artworkId: jsonComment["artworkId"],
+                        content: jsonComment["content"]
+                    }
+                    this.commentsData.push(eachComment)
                 }
-                this.commentsData.push(eachComment)
             }
-        }            
             )
-        }
-    
-    
-    addComment(){            
+    }
+
+
+    addComment() {
         console.log('The create comment request is sent!')
         this.commentService.addComment(this.createNewComment())
-        .subscribe({
-            next: (data:any) => { console.log(data) }, // completeHandler
-            error: (error:any) => { console.log(error)  },    // errorHandler                         
-        });
-              
-    }   
+            .subscribe(response => {
+                // Handle success response
+                console.log('Response:', response);
+                // Reload the page
+
+            });
+        location.reload()
+    }
 
 
-    createNewComment():ArtworkNewComment{
-        return {            
-            content:this.commentInput,
-            artworkId:this.artwork?.id as number
+    createNewComment(): ArtworkNewComment {
+        return {
+            content: this.commentInput,
+            artworkId: this.artwork?.id as number
         }
     }
 
