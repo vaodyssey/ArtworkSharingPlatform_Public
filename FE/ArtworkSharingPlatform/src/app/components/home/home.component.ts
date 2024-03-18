f7e364715586dc04c6c1c1edfea78885c4ae4c3e
vathuglife <thuandt582@gmail.com>
2024-03-18 14:33:30 +0700

    vathuglife: added Artwork Carousels for remaining Artwork Genres

--- FE/ArtworkSharingPlatform/src/app/components/home/home.component.ts
+++ FE/ArtworkSharingPlatform/src/app/components/home/home.component.ts
@@ -12,18 +12,51 @@ export class HomeComponent implements OnInit {
   pageNumber: number = 1
   pageSize: number = 15
   animeArtworks: Artwork[] = [];
+  landscapeArtworks: Artwork[] = [];
+  fictionArtworks: Artwork[] = [];
+  portraitArtworks: Artwork[] = [];
 
   constructor(private artworkService: ArtworkService) {
-    this.getAnimeArtworks()
+    // this.getAnimeArtworks()
+    // this.getLandscapeArtworks()
+    // this.getFictionArtworks()
+    // this.getPortraitArtworks()
+    this.getArtworkData()
   }
   ngOnInit(): void {
 
   }
-  async getAnimeArtworks() {
+
+  async getArtworkData() {
     this.artworkService.getArtworksByGenreId(1, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: landscapeArtworks => {
+          this.landscapeArtworks = landscapeArtworks;
+        }
+      }
+      );
+
+    this.artworkService.getArtworksByGenreId(2, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: portraitArtworks => {
+          this.portraitArtworks = portraitArtworks;
+        }
+      }
+      );
+
+    this.artworkService.getArtworksByGenreId(3, this.pageNumber, this.pageSize)
       .subscribe({
         next: animeArtworks => {
           this.animeArtworks = animeArtworks;
+        }
+      }
+      );
+
+
+    this.artworkService.getArtworksByGenreId(4, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: fictionArtworks => {
+          this.fictionArtworks = fictionArtworks;
         }
       }
       );
