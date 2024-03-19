0f63c9f4b8512b298636ffae43581abca106773d
alannguyen2003 <nguyenho30112003@gmail.com>
2024-03-20 00:36:00 +0700

    add view commission history for audience

--- FE/ArtworkSharingPlatform/src/app/components/user/commissions/commissions.component.ts
+++ FE/ArtworkSharingPlatform/src/app/components/user/commissions/commissions.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {CommissionHistoryAdmin} from "../../../_model/commissionHistoryAdmin.model";
+import {Subject} from "rxjs";
+import {AdminService} from "../../../_services/admin.service";
+import {CommissionService} from "../../../_services/commission.service";
+import {CommissionHistoryAudience} from "../../../_model/commissionHistoryAudience.model";
 
 @Component({
   selector: 'app-commissions',
@@ -6,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./commissions.component.css']
 })
 export class CommissionsComponent {
+  commissionsItem: CommissionHistoryAudience;
+  dtOptions: DataTables.Settings = {
+    pagingType: 'full_numbers'
+  }
+  dtTrigger: Subject<any> = new Subject<any>();
 
+  constructor(private commissionService: CommissionService) {}
+
+  ngOnInit() {
+    this.loadAllCommissions();
+  }
+
+  loadAllCommissions() {
+    this.commissionService.getCommissions().subscribe(response => {
+      this.commissionsItem = response;
+      this.dtTrigger.next(null);
+    });
+  }
 }
