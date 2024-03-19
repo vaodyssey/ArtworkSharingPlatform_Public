423266b3a92b82889e2fc4b23a86cd5c43cdacad
alannguyen2003 <nguyenho30112003@gmail.com>
2024-03-19 20:26:20 +0700

    fix routing in guard authentication

--- FE/ArtworkSharingPlatform/src/app/_guards/auth.guard.ts
+++ FE/ArtworkSharingPlatform/src/app/_guards/auth.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {AccountService} from "../_services/account.service";
 import {inject} from "@angular/core";
 import {ToastrService} from "ngx-toastr";
@@ -7,12 +7,14 @@ import {map} from "rxjs";
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
   const toastr = inject(ToastrService);
+  const router = inject(Router);
 
   return accountService.currentUser$.pipe(
     map(user => {
       if (user) return true;
       else {
         toastr.error("You are not logged in to enter this page");
+        router.navigate(['/login']);
         return false;
       }
     })
